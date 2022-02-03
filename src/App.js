f6f8cd7cc3ff05c85ef2f2bb8aaa88918022877b
Tasleemofx <oladepotesleem5@gmail.com>
2022-02-03 14:13:09 +0100

    changed html file and deleted some files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-
-
+import React, {useState} from 'react';
 import './App.css';
-
+import {localData} from "./data/data"
 function App() {
+  const [data, setData] = useState(localData);
+  console.log(data)
   return (
     <div className="App">
       Hello redux      
