77035160fd5a8554529c605a0534fc2df4effd99
Tasleemofx <oladepotesleem5@gmail.com>
2022-02-05 02:16:55 +0100

    Tables destructured

---
+++ src/components/Tables.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
+
+const Tables = ({adminUsers, handleDelete}) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead className="table-head">
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Username</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Address</TableCell>
+            <TableCell align="center">Edit</TableCell>
+            <TableCell align="center">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {adminUsers.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell align="center">{row.id}</TableCell>
+              <TableCell align="center">{row.name}</TableCell>
+              <TableCell align="center">{row.username}</TableCell>
+              <TableCell align="center">{row.email}</TableCell>
+              <TableCell align="center">{row.address.city}</TableCell>
+              <TableCell align="center">
+                <Link to="/EditUser/:id">
+                  <button className="edit">Edit</button>
+                </Link>
+              </TableCell>
+              <TableCell>
+                <button className="delete" onClick={() => handleDelete(row.id)}>
+                  Delete
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default Tables;
--- src/features/admin/adminSlice.js
+++ src/features/admin/adminSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { localData } from '../../data/data';
 export const adminSlice = createSlice({
     name: 'admin',
